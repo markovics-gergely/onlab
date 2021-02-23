bb83babc92e4833e9f4283d28ed5ff3030058970
nagyviktor <nagy.viktor99@gmail.com>
2021-02-23 14:59:04 +0100

    opencv

--- Code/src/logic/Main.java
+++ Code/src/logic/Main.java
@@ -1,8 +1,11 @@
 package logic;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("lol");
+	    System.out.println("Verzió: " + Core.VERSION);
     }
 }
