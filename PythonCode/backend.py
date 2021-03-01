345d8303eb0495800a906ef37ef822181977744f
markovics-gergely <61557724+markovics-gergely@users.noreply.github.com>
2021-03-01 17:07:12 +0100

    Update backend.py

--- PythonCode/backend.py
+++ PythonCode/backend.py
@@ -84,37 +84,48 @@ def webcamFaceDetect():
     video_capture.release()
     cv2.destroyAllWindows()
 
-age_model = cv2.dnn.readNetFromCaffe("age.prototxt", "dex_imdb_wiki.caffemodel")
+age_model = cv2.dnn.readNetFromCaffe("age.prototxt", "dex_chalearn_iccv2015.caffemodel")
 gender_model = cv2.dnn.readNetFromCaffe("gender.prototxt", "gender.caffemodel")
 haar_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-output_indexes = np.array([i for i in range(0, 101)])
+
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+ageList=[[0, 2], [4, 6], [8, 12], [15, 20], [25, 32], [38, 43], [48, 53], [60, 100]]
+genderList = ['Female', 'Male']
+
+def find_interval(num):
+    for intervals in ageList:
+      if intervals[0] <= num <= intervals[-1]:
+         return (intervals[0], intervals[-1])
+         
 def ipcamFaceDetect():
     face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
     video = cv2.VideoCapture(0)
     address = 'http://192.168.1.100:8080/video'
     video.open(address)
-
+   
     while True:
         check, frame = video.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
-        for face in faces:
-            x, y, w, h = face
-            detected_face = frame[int(y):int(y+h), int(x):int(x+w)]
-            detected_face = cv2.resize(detected_face, (224, 224))
-            img_blob = cv2.dnn.blobFromImage(detected_face) #caffe model expects (1, 3, 224, 224) shape input
+        if frame is not None:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+            for face in faces:
+                  x, y, w, h = face
+                  timedelay = time.time()
+                  detected_face = frame[int(y):int(y+h), int(x):int(x+w)].copy()
+                  img_blob = cv2.dnn.blobFromImage(detected_face, 1, (224, 224), MODEL_MEAN_VALUES, swapRB=False)
+                  
+                  age_model.setInput(img_blob)
+                  age_pred = age_model.forward()
+                  age = str(find_interval(age_pred[0].argmax()))
 
-            age_model.setInput(img_blob)
-            age_dist = age_model.forward()[0]
-            apparent_predictions = round(np.sum(age_dist * output_indexes), 2)
-            gender_model.setInput(img_blob)
-            gender_class = gender_model.forward()[0]
-            gender = 'Woman ' if np.argmax(gender_class) == 0 else 'Man'
+                  gender_model.setInput(img_blob)
+                  gender_class = gender_model.forward()[0]
+                  gender = genderList[np.argmax(gender_class)]
 
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            cv2.putText(frame, str(apparent_predictions), (x+int(3*w/4), y - 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 111, 255), 2)
-            cv2.putText(frame, gender, (x+int(w/4), y - 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 111, 255), 2)
+                  cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+                  cv2.putText(frame, age, (x+int(3*w/4), y - 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+                  cv2.putText(frame, gender, (x+int(w/4), y - 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
         
         cv2.imshow("IP Cam Facedetection", frame)
 
