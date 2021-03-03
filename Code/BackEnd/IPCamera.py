eb03607b162c8fa1ce8e9143d5395ec24b09ff69
nagyviktor <nagy.viktor99@gmail.com>
2021-03-03 23:52:48 +0100

    lol

--- Code/BackEnd/IPCamera.py
+++ Code/BackEnd/IPCamera.py
@@ -13,14 +13,14 @@ genderList = ['Female', 'Male']
 
 def str_interval(num):
     for intervals in ageList:
-      if intervals[0] <= num <= intervals[-1]:
-         return str(intervals[0]) + "-" + str(intervals[-1])
+        if intervals[0] <= num <= intervals[-1]:
+            return str(intervals[0]) + "-" + str(intervals[-1])
 
 def find_interval_id(num):
     i = 0
     for i in range(len(ageList)) :
         if ageList[i][0] <= num <= ageList[i][-1]:
-         return i
+            return i
 
 def ipcamFaceDetect(urladdress):
     age_model = cv2.dnn.readNetFromCaffe("age.prototxt", "dex_chalearn_iccv2015.caffemodel")
@@ -38,24 +38,24 @@ def ipcamFaceDetect(urladdress):
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             faces = haar_detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
             for face in faces:
-                  x, y, w, h = face
-                  timedelay = time.time()
-                  detected_face = frame[int(y):int(y+h), int(x):int(x+w)].copy()
-                  img_blob = cv2.dnn.blobFromImage(detected_face, 1, (224, 224), MODEL_MEAN_VALUES, swapRB=False)
+                x, y, w, h = face
+                timedelay = time.time()
+                detected_face = frame[int(y):int(y+h), int(x):int(x+w)].copy()
+                img_blob = cv2.dnn.blobFromImage(detected_face, 1, (224, 224), MODEL_MEAN_VALUES, swapRB=False)
                   
-                  age_model.setInput(img_blob)
-                  age_pred = age_model.forward()
-                  agenum = age_pred[0].argmax()
-                  age = str_interval(agenum)
-                  ageBucket[find_interval_id(agenum)] += 1
+                age_model.setInput(img_blob)
+                age_pred = age_model.forward()
+                agenum = age_pred[0].argmax()
+                age = str_interval(agenum)
+                ageBucket[find_interval_id(agenum)] += 1
 
-                  gender_model.setInput(img_blob)
-                  gender_class = gender_model.forward()[0]
-                  gender = genderList[np.argmax(gender_class)]
+                gender_model.setInput(img_blob)
+                gender_class = gender_model.forward()[0]
+                gender = genderList[np.argmax(gender_class)]
 
-                  cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
-                  cv2.putText(frame, age, (x+int(3*w/4), y - 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
-                  cv2.putText(frame, gender, (x+int(w/4), y - 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+                cv2.putText(frame, age, (x+int(3*w/4), y - 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+                cv2.putText(frame, gender, (x+int(w/4), y - 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
         
         cv2.imshow("IP Cam Facedetection", frame)
 
