ed99def4217b2d385ae38c452f09697568374bfa
markovics-gergely <61557724+markovics-gergely@users.noreply.github.com>
2021-03-10 14:31:40 +0100

    Create IPManager.py

---
+++ Code/BackEnd/IPManager.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2, time
+import urllib.request as urllib
+import face_recognition as fr
+from datetime import datetime
+import threading
+import json
+import pandas as pd
+import os.path
+
+class IPManager:
+    def __init__(self):
+        self.cameraList = parseCameras()
+
+    def addCamera(self, ipaddr):
+
+    def deleteCamera(self, ipaddr):
+
+    def startCamera(self, ipaddr):
+
+    def stopCamera(self, ipaddr):
+   
+    def parseCameras():
+
+    
