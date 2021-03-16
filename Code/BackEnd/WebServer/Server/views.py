28bf73d459feb4ebf83925e1e2459e3365e0e860
Nagy Viktor <nagy.viktor99@gmail.com>
2021-03-16 15:15:36 +0100

    Django elkezdve

---
+++ Code/BackEnd/WebServer/Server/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def prediction(request):
+    return render(request, 'prediction.html')
+
+def results(request):
+    return render(request, 'results.html')
