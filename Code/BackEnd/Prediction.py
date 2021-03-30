edef7246e97583cb7e44f4f0e22d7c697dcc00f2
Nagy Viktor <52136313+nagyviktor@users.noreply.github.com>
2021-03-30 02:17:02 +0200

    Példa a prophethez

--- Code/BackEnd/Prediction.py
+++ Code/BackEnd/Prediction.py
@@ -2,10 +2,37 @@ import pandas as pd
 from fbprophet import Prophet
 import pystan
 
-df = pd.read_csv('DB/cameras/192-168-0-100-8080.csv')
+
+''' #A PYSTAN ELLENŐRZÉSÉHEZ KOMMENTEZD KI
+model_code = 'parameters {real y;} model {y ~ normal(0,1);}'
+model = pystan.StanModel(model_code=model_code)
+y = model.sampling().extract()['y']
+y.mean()  # with luck the result will be near 0
+'''
+
+#Ez egy random kód a netről amihez van tutorial, csak beraktam hozzá a dataset.csv-t mert azt használja
+#Az hogy az elején miért írja ki hogy "Importing plotly failed. Interactive plots will not work." fogalmam sincs
+
+df = pd.read_csv('DB/dataset.csv')
+
+df['Year'] = df['Time Date'].apply(lambda x: str(x)[-4:])
+df['Month'] = df['Time Date'].apply(lambda x: str(x)[-6:-4])
+df['Day'] = df['Time Date'].apply(lambda x: str(x)[:-6])
+df['ds'] = pd.DatetimeIndex(df['Year']+'-'+df['Month']+'-'+df['Day'])
+
+df = df.loc[(df['Product']==2667437) & (df['Store']=='QLD_CW_ST0203')]
+df.drop(['Time Date', 'Product', 'Store', 'Year', 'Month', 'Day'], axis=1, inplace=True)
+df.columns = ['y', 'ds']
+
 df.head()
 
-m = Prophet()
-m.params()
+m = Prophet(interval_width=0.95, daily_seasonality=True)
+model = m.fit(df)
 
+future = m.make_future_dataframe(periods=100,freq='D')
+forecast = m.predict(future)
+forecast.head()
 
+plot1 = m.plot(forecast)
+
+plt2 = m.plot_components(forecast)
